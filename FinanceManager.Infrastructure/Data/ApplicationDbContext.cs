50077bdc44ece9993544d9996d7e150500fdac97
Prabhu Kandel <kandelprabhu111@gmail.com>
2025-08-27 17:02:08 +0545

    Add project files.

---
+++ FinanceManager.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FinanceManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManager.Infrastructure.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        { }
+        public DbSet<TransactionCategory> TransactionCategories { get; set; }
+
+    }
+}
