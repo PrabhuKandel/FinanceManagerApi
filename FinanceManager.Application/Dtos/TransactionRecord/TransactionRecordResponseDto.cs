b912de6116ac994548f0c19df8e896ee6ae781b4
Prabhu Kandel <kandelprabhu111@gmail.com>
2025-09-05 19:34:12 +0545

    refactor(transaction-record-response): hiding null in response

--- FinanceManager.Application/Dtos/TransactionRecord/TransactionRecordResponseDto.cs
+++ FinanceManager.Application/Dtos/TransactionRecord/TransactionRecordResponseDto.cs
@@ -7,41 +7,42 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using FinanceManager.Application.Dtos.PaymentMethod;
 using FinanceManager.Application.Dtos.TransactionCategory;
+using Newtonsoft.Json;
 
 namespace FinanceManager.Application.Dtos.TransactionRecord
 {
 
-    public class EntitySummaryDto
-    {
-        public Guid Id { get; set; }
-        public required string Name { get; set; }
-    }
+        public class EntitySummaryDto
+        {
+            public Guid Id { get; set; }
+            public required string Name { get; set; }
+        }
 
-    public class ApplicationUserSummaryDto
-    {
-        public required string Id { get; set; }
-        public required string FirstName { get; set; }
+        public class ApplicationUserSummaryDto
+        {
+            public required string Id { get; set; }
+            public required string FirstName { get; set; }
 
-    }
-    public class TransactionRecordResponseDto
-    {
+        }
+        public class TransactionRecordResponseDto
+        {
 
-        public Guid Id { get; set; }
-        public EntitySummaryDto? TransactionCategory { get; set; }
-        public EntitySummaryDto? PaymentMethod { get; set; }
-        public decimal Amount { get; set; }
-        public string? Description { get; set; }
-        public DateTime TransactionDate { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+            public Guid Id { get; set; }
+            public EntitySummaryDto? TransactionCategory { get; set; }
+            public EntitySummaryDto? PaymentMethod { get; set; }
+            public decimal Amount { get; set; }
+            public string? Description { get; set; }
+            public DateTime TransactionDate { get; set; }
+            public DateTime CreatedAt { get; set; }
+            public DateTime UpdatedAt { get; set; }
 
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public ApplicationUserSummaryDto? CreatedBy { get; set; }
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public ApplicationUserSummaryDto? CreatedBy { get; set; }
 
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public ApplicationUserSummaryDto? UpdatedBy { get; set; }
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]    
+            public ApplicationUserSummaryDto? UpdatedBy { get; set; }
 
-    }
+        }
 
     //public class AdminTransactionRecordResponseDto
     //{
