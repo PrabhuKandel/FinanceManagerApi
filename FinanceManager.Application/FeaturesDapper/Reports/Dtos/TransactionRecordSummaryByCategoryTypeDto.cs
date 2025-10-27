81f86c24a61167f9b2f77f87d54b1bb34a071904
Prabhu Kandel <kandelprabhu111@gmail.com>
2025-10-27 11:25:37 +0545

    feat(report):Add transaction summary report API and stored procedure with period-based Income/Expense

---
+++ FinanceManager.Application/FeaturesDapper/Reports/Dtos/TransactionRecordSummaryByCategoryTypeDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace FinanceManager.Application.FeaturesDapper.Reports.Dtos
+{
+    public class TransactionRecordSummaryByCategoryTypeDto
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+
+        public decimal NetBalance { get; set; }
+    }
+}
---
+++ FinanceManager.Application/FeaturesDapper/Reports/Queries/TransactionRecordSummaryByCategoryType/TransactionRecordSummaryByCategoryTypeQuery.cs
@@ -0,0 +1,18 @@
+﻿
+
+using FinanceManager.Application.Common;
+using FinanceManager.Application.FeaturesDapper.Reports.Dtos;
+using FinanceManager.Application.FeaturesDapper.Reports.Enums;
+using MediatR;
+
+namespace FinanceManager.Application.FeaturesDapper.Reports.Queries.TransactionRecordSummaryByCategoryType
+{
+    public record TransactionRecordSummaryByCategoryTypeQuery
+        (
+        DateTime? FromDate
+        ,DateTime? ToDate,
+        Period GroupBy = Period.Month
+        ) :IRequest<OperationResult<IEnumerable<TransactionRecordSummaryByCategoryTypeDto>>>
+    {
+    }
+}
