2333eab7aafcd51f98214891f3d0b0462e0096ee
Prabhu Kandel <kandelprabhu111@gmail.com>
2025-11-10 16:51:12 +0545

    feat(auth): implement password reset via email and token verification

---
+++ FinanceManager.Application/Features/Auth/ForgotPassword/Commands/ForgotPasswordCommand.cs
@@ -0,0 +1,16 @@
+﻿
+
+using FinanceManager.Application.Common;
+using MediatR;
+
+namespace FinanceManager.Application.Features.Auth.ForgotPassword.Commands
+{
+    public record ForgotPasswordCommand
+        (
+            string Email,
+            string ClientURI
+        ) : IRequest<OperationResult<string>>
+    {
+     
+    }
+}
---
+++ FinanceManager.Application/Features/Auth/ForgotPassword/Commands/ForgotPasswordHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq.Expressions;
+using Ardalis.GuardClauses;
+using FinanceManager.Application.Common;
+using FinanceManager.Application.Interfaces.Services;
+using FinanceManager.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinanceManager.Application.Features.Auth.ForgotPassword.Commands
+{
+    public class ForgotPasswordHandler(UserManager<ApplicationUser> _userManager, IEmailService _emailService) : IRequestHandler<ForgotPasswordCommand, OperationResult<string>>
+    {
+        public async Task<OperationResult<string>> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user  = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return new OperationResult<string>
+                {
+                    Message = "Password reset link has been sent to your email."
+                };
+            }
+
+            //generate password reset token
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user!);
+            Console.WriteLine(token);
+
+            // Build reset link for frontend
+            var resetLink = $"{request.ClientURI}?email={user.Email}&token={Uri.EscapeDataString(token)}";
+
+            await _emailService.SendEmailAsync(
+                to: user.Email,
+                subject: "Password Reset Request",
+                body: $"Click the link to reset your password: {resetLink}"
+            );
+
+            return new OperationResult<string>
+            {
+                Message = "Password reset link has been sent to your email."
+            };
+        }
+    }
+}
